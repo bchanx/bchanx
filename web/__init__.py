a1338745d5d3087185cdae6c66a4b444d5c925e9
Brian Chan <bchanx@greplin.com>
2013-02-10 18:31:16 -0800

    postgres db boilerplate

--- web/__init__.py
+++ web/__init__.py
@@ -5,7 +5,11 @@ Contact: bchanx@gmail.com
 '''
 
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
 app = Flask(import_name='web')
+app.config.from_object('config')
+db = SQLAlchemy(app)
 
 from web import errors, index, fantasy, jukebox
 
