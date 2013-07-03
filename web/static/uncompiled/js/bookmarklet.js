f4224bf8b8c63aa53c9e5fb494bc87aa4211500d
Brian Chan <bchanx@greplin.com>
2013-07-03 01:50:50 -0700

    Use https

--- web/static/uncompiled/js/bookmarklet.js
+++ web/static/uncompiled/js/bookmarklet.js
@@ -5,5 +5,5 @@
 //
 
 // JUKEBOX 
-javascript:(function(){function addVideo(){window.location.href='http://www.bchanx.com/jukebox/playlistAddMedia?key='+CryptoJS.HmacMD5(location.href,"<key>")+'&pid=1&media-url='+encodeURIComponent(location.href);}script=document.createElement('script');script.src='http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/hmac-md5.js';script.onload=addVideo;document.body.appendChild(script);})();
+javascript:(function(){function addVideo(){window.location.href='http://www.bchanx.com/jukebox/playlistAddMedia?key='+CryptoJS.HmacMD5(location.href,"<key>")+'&pid=1&media-url='+encodeURIComponent(location.href);}script=document.createElement('script');script.src='https://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/hmac-md5.js';script.onload=addVideo;document.body.appendChild(script);})();
 
