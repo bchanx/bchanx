38927ac7a573ec979d7ad3376ad693394b176ff7
Brian Chan <bchanx@greplin.com>
2013-09-03 02:21:36 -0700

    Handle more git patterns in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Deploy script for heroku
+# Deploy script for heroku!
 
 function printStatus {
   echo -e "\033[1;32m--- $1 ---\033[0m"
@@ -25,15 +25,36 @@ fi
 # Push changes to Git
 #####
 status=$(git status 2>&1)
-if [[ ! $status =~ "nothing to commit" ]]; then
-  printStatus "Pushing changes to Github"
-  read -e -p "Please enter a commit message (empty to abort): " COMMIT_MSG
-  if [[ -z "$COMMIT_MSG" ]]; then
-    printError "No commit message, aborting."
-    exit 1
+if [[ ! $status =~ "nothing to commit, working directory clean" ]]; then
+  if [[ $status =~ "Untracked files:" ]]; then
+    read -e -p "Untracked files present, continue? (y/n): " UNTRACKED
+    if [[ -z "$UNTRACKED" ]] || [[ $UNTRACKED == "n" ]] || [[ $UNTRACKED == "N" ]]; then
+      printError "Untracked files present, aborting."
+      exit 1
+    fi
   fi
-  git commit -a -m "$COMMIT_MSG"
-  git push origin master
+  if [[ $status =~ "Changes not staged for commit:" ]]; then
+    read -e -p "Unstaged changes, add them? (y/n): " UNSTAGED
+    if [[ -z "$UNSTAGED" ]] || [[ $UNSTAGED == "n" ]] || [[ $UNSTAGED == "N" ]]; then
+      UNSTAGED=false
+    else 
+      UNSTAGED=true
+    fi
+  fi
+  if [[ $status =~ "Changes to be committed" ]] || $UNSTAGED; then
+    printStatus "Pushing changes to Github"
+    read -e -p "Please enter a commit message (empty to abort): " COMMIT_MSG
+    if [[ -z "$COMMIT_MSG" ]]; then
+      printError "No commit message, aborting."
+      exit 1
+    fi
+    if $UNSTAGED; then
+      git commit -a -m "$COMMIT_MSG"
+    else
+      git commit -m "$COMMIT_MSG"
+    fi
+    git push origin master
+  fi
 fi
 
 #####
