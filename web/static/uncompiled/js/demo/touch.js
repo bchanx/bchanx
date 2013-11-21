a91f65a057f076c965106d8495a2670eb2259f65
bchanx <bchanx@gmail.com>
2013-11-20 17:19:55 -0800

    More testing

--- web/static/uncompiled/js/demo/touch.js
+++ web/static/uncompiled/js/demo/touch.js
@@ -7,10 +7,13 @@ $(function() {
  
   var start = {};
   var delta = {};
-  var isScrolling = undefined;
   var width = $('#touch').width();
+  var height = $('#touch').height();
+  var touch = document.getElementById('touch');
 
-  $('#touch').bind('touchstart', function(e) {
+  console.log("-->> OK");
+  touch.addEventListener('touchstart', function(e) {
+    $('#dir').text('');
     $('#touch').css('border-color', 'green').text('touchstart');
     var touches = e.touches[0];
     start = {
@@ -23,9 +26,7 @@ $(function() {
     $('#time').text(start.time);
     delta = {};
 
-    isScrolling = undefined;
-
-    $('#touch').bind('touchmove', function(e) {
+    touch.addEventListener('touchmove', function(e) {
       $('#touch').text('touchmove');
       if (e.touches.length > 1 || e.scale && e.scale !== 1) return;
       var touches = e.touches[0];
@@ -36,36 +37,28 @@ $(function() {
       $('#deltax').text(delta.x);
       $('#deltay').text(delta.y);
 
-      if (typeof isScrolling == 'undefined') {
-        isScrolling = !!( isScrolling || Math.abs(delta.x) < Math.abs(delta.y) );
-      }
-      $('#scrolling').text(isScrolling);
-
-      if (!isScrolling) {
-        e.preventDefault();
-      }
+      e.preventDefault();
     });
 
-    $('#touch').bind('touchend', function(e) {
+    touch.addEventListener('touchend', function(e) {
       $('#touch').text('touchend');
-       var duration = +new Date - start.time;
-       var isValidSlide = Number(duration) < 250 && Math.abs(delta.x) > 20 || Math.abs(delta.x) > width/2;
-       var right = delta.x < 0;
-       if (!isScrolling && isValidSlide) {
-         if (right) {
-           $('#right').text('YEAH!');
-           // SLIDE RIGHT!
-         } else {
-            // SLIDE LEFT!
-           $('#left').text('YEAH!');
-         }
-       }
-       $('#touch').unbind('touchmove');
-       $('#touch').unbind('touchend');
-       $('#touch').css('border-color', 'black');
+      var duration = +new Date - start.time;
+      $('#duration').text(duration);
+      var validH = Number(duration) < 250 && Math.abs(delta.x) > 20 || Math.abs(delta.x) > width/2;
+      var validV = Number(duration) < 250 && Math.abs(delta.y) > 20 || Math.abs(delta.y) > height/2;
+      $('#valid').text(validH || validV);
+      if (validH || validV) {
+        if (validH) {
+          $('#dir').text((delta.x < 0) ? '#right' : '#left');
+        } else {
+          $('#dir').text((delta.y < 0) ? '#down' : '#up');
+        }
+      }
+      $('#touch').unbind('touchmove');
+      $('#touch').unbind('touchend');
+      $('#touch').css('border-color', 'black');
     });
 
   });
-
 
 });
