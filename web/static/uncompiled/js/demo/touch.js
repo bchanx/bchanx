68873cda064b96f2a03a731ed58e6582b8076cfa
bchanx <bchanx@gmail.com>
2013-11-20 18:32:42 -0800

    Moar

--- web/static/uncompiled/js/demo/touch.js
+++ web/static/uncompiled/js/demo/touch.js
@@ -24,37 +24,33 @@ $(function() {
     $('#deltax').text(delta.x);
     $('#deltay').text(delta.y);
 
-    var duration = +new Date - start.time;
-    if (Number(duration) >= 250) {
-      touch.removeEventListener('touchmove', moveListener);
-      touch.removeEventListener('touchend', endListener);
+    if (Number(+new Date - start.time) >= 250) {
       $('#touch').css('border-color', 'black');
-      return true;
+      return;
     }
-
     e.preventDefault();
-
   };
 
   var endListener = function(e) {
+    console.log("-->> END LISTENER!");
     $('#touch').text('touchend');
-    var duration = +new Date - start.time;
-    $('#duration').text(duration);
-    var dx = Math.abs(delta.x);
-    var dy = Math.abs(delta.y);
-    var validH = dx > 20;
-    var validV = dy > 20;
-    var dirH = delta.x < 0 ? 'right': 'left';
-    var dirV = delta.y < 0 ? 'down' : 'up';
-    $('#valid').text(validH || validV);
-    var dir = (validH && validV ? (dx > dy ? dirH : dirV) : (validH ? dirH : (validV ? dirV : null))); 
-    if (dir) $('#dir').text(dir);
-    touch.removeEventListener('touchmove', moveListener);
-    touch.removeEventListener('touchend', endListener);
-    $('#touch').css('border-color', 'black');
+    if (Number(+new Date - start.time) < 250) {
+      $('#duration').text(duration);
+      var dx = Math.abs(delta.x);
+      var dy = Math.abs(delta.y);
+      var validH = dx > 20;
+      var validV = dy > 20;
+      var dirH = delta.x < 0 ? 'right': 'left';
+      var dirV = delta.y < 0 ? 'down' : 'up';
+      $('#valid').text(validH || validV);
+      var dir = (validH && validV ? (dx > dy ? dirH : dirV) : (validH ? dirH : (validV ? dirV : null))); 
+      if (dir) $('#dir').text(dir);
+      $('#touch').css('border-color', 'black');
+    }
   };
 
   touch.addEventListener('touchstart', function(e) {
+    console.log("-->> START LISTENER!");
     $('#dir').text('');
     $('#touch').css('border-color', 'green').text('touchstart');
     var touches = e.touches[0];
