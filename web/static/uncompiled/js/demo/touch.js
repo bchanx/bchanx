cfac69c4f72a4f98c6862da4063366ccf1cf64b4
bchanx <bchanx@gmail.com>
2013-11-20 18:07:47 -0800

    Go

--- web/static/uncompiled/js/demo/touch.js
+++ web/static/uncompiled/js/demo/touch.js
@@ -12,6 +12,46 @@ $(function() {
   var touch = document.getElementById('touch');
 
   console.log("-->> OK");
+
+  var moveListener = function(e) {
+    $('#touch').text('touchmove');
+    if (e.touches.length > 1 || e.scale && e.scale !== 1) return;
+    var touches = e.touches[0];
+    delta = {
+      x: touches.pageX - start.x,
+      y: touches.pageY - start.y
+    };
+    $('#deltax').text(delta.x);
+    $('#deltay').text(delta.y);
+
+    e.preventDefault();
+
+    var duration = +new Date - start.time;
+    if (Number(duration) >= 250) {
+      touch.removeEventListener('touchmove', moveListener);
+      touch.removeEventListener('touchend', endListener);
+      $('#touch').css('border-color', 'black');
+    }
+  };
+
+  var endListener = function(e) {
+    $('#touch').text('touchend');
+    var duration = +new Date - start.time;
+    $('#duration').text(duration);
+    var dx = Math.abs(delta.x);
+    var dy = Math.abs(delta.y);
+    var validH = dx > 20;
+    var validV = dy > 20;
+    var dirH = delta.x < 0 ? 'right': 'left';
+    var dirV = delta.y < 0 ? 'down' : 'up';
+    $('#valid').text(validH || validV);
+    var dir = (validH && validV ? (dx > dy ? dirH : dirV) : (validH ? dirH : (validV ? dirV : null))); 
+    if (dir) $('#dir').text(dir);
+    touch.removeEventListener('touchmove', moveListener);
+    touch.removeEventListener('touchend', endListener);
+    $('#touch').css('border-color', 'black');
+  };
+
   touch.addEventListener('touchstart', function(e) {
     $('#dir').text('');
     $('#touch').css('border-color', 'green').text('touchstart');
@@ -26,39 +66,8 @@ $(function() {
     $('#time').text(start.time);
     delta = {};
 
-    touch.addEventListener('touchmove', function(e) {
-      $('#touch').text('touchmove');
-      if (e.touches.length > 1 || e.scale && e.scale !== 1) return;
-      var touches = e.touches[0];
-      delta = {
-        x: touches.pageX - start.x,
-        y: touches.pageY - start.y
-      };
-      $('#deltax').text(delta.x);
-      $('#deltay').text(delta.y);
-
-      e.preventDefault();
-    });
-
-    touch.addEventListener('touchend', function(e) {
-      $('#touch').text('touchend');
-      var duration = +new Date - start.time;
-      $('#duration').text(duration);
-      var validH = Number(duration) < 250 && Math.abs(delta.x) > 20 || Math.abs(delta.x) > width/2;
-      var validV = Number(duration) < 250 && Math.abs(delta.y) > 20 || Math.abs(delta.y) > height/2;
-      $('#valid').text(validH || validV);
-      if (validH || validV) {
-        if (validH) {
-          $('#dir').text((delta.x < 0) ? '#right' : '#left');
-        } else {
-          $('#dir').text((delta.y < 0) ? '#down' : '#up');
-        }
-      }
-      $('#touch').unbind('touchmove');
-      $('#touch').unbind('touchend');
-      $('#touch').css('border-color', 'black');
-    });
-
+    touch.addEventListener('touchmove', moveListener);
+    touch.addEventListener('touchend', endListener);
   });
 
 });
