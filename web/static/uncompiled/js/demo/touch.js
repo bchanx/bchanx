862bcd3d41448801fa47a42db05bbd2b080c53b1
bchanx <bchanx@gmail.com>
2013-11-20 19:05:44 -0800

    again

--- web/static/uncompiled/js/demo/touch.js
+++ web/static/uncompiled/js/demo/touch.js
@@ -17,18 +17,15 @@ $(function() {
     $('#touch').text('touchmove');
     if (e.touches.length > 1 || e.scale && e.scale !== 1) return;
     var touches = e.touches[0];
-    delta = {
-      x: touches.pageX - start.x,
-      y: touches.pageY - start.y
-    };
+    delta.x = touches.pageX - start.x;
+    delta.y = touches.pageY - start.y;
     $('#deltax').text(delta.x);
     $('#deltay').text(delta.y);
-
-    if (Math.abs(delta.x) + Math.abs(delta.y) < 40 && Number(+new Date - start.time) >= 150)
+    if (Number(+new Date - start.time) < 250) {
+      e.preventDefault();
+    } else {
       $('#touch').css('border-color', 'black');
       return;
-    } else {
-      e.preventDefault();
     }
   };
 
@@ -36,24 +33,23 @@ $(function() {
     console.log("-->> END LISTENER!");
     $('#touch').text('touchend');
     var duration = Number(+new Date - start.time);
-    if (duration < 250) {
-      $('#duration').text(duration);
-      var dx = Math.abs(delta.x);
-      var dy = Math.abs(delta.y);
-      var validH = duration < 250 && dx > 20 || dx > width/2;
-      var validV = duration < 250 && dy > 20 || dy > height/2;
-      var dirH = delta.x < 0 ? 'right': 'left';
-      var dirV = delta.y < 0 ? 'down' : 'up';
-      $('#valid').text(validH || validV);
-      var dir = (validH && validV ? (dx > dy ? dirH : dirV) : (validH ? dirH : (validV ? dirV : null))); 
-      if (dir) $('#dir').text(dir);
-      $('#touch').css('border-color', 'black');
-    }
+    $('#duration').text(duration);
+    var dx = Math.abs(delta.x);
+    var dy = Math.abs(delta.y);
+    var validH = duration < 250 && dx > 20 || dx > width/2;
+    var validV = duration < 250 && dy > 20 || dy > height/2;
+    var dirH = delta.x < 0 ? 'right': 'left';
+    var dirV = delta.y < 0 ? 'down' : 'up';
+    $('#valid').text(validH || validV);
+    var dir = (validH && validV ? (dx > dy ? dirH : dirV) : (validH ? dirH : (validV ? dirV : null))); 
+    if (dir) $('#dir').text(dir);
+    $('#touch').css('border-color', 'black');
   };
 
   touch.addEventListener('touchstart', function(e) {
     console.log("-->> START LISTENER!");
     $('#dir').text('');
+    $('#valid').text('');
     $('#touch').css('border-color', 'green').text('touchstart');
     var touches = e.touches[0];
     start = {
@@ -64,7 +60,8 @@ $(function() {
     $('#x').text(start.x);
     $('#y').text(start.y);
     $('#time').text(start.time);
-    delta = {};
+    delta.x = 0;
+    delta.y = 0;
 
     touch.addEventListener('touchmove', moveListener);
     touch.addEventListener('touchend', endListener);
