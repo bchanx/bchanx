bf89f04d30b456f16dea237a132076ec61ed385d
bchanx <bchanx@gmail.com>
2015-09-30 04:27:18 -0700

    GAMEBOY

---
+++ web/static/compiled/demo-gameboy-css.js
@@ -0,0 +1,6 @@
+/*!
+ * Copyright (c) 2014 Brian Chan (bchanx.com)
+ * All Rights Reserved.
+ */
+
+var bchanx=bchanx||{};$(function(){console.log("hi");$("#button").bind("click",function(){$("#gameboy").hide(50,function(){setTimeout(function(){$("#gameboy").show()},100)})})});
