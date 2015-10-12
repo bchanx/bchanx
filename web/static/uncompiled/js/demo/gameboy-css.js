1f51b633e364bea3452b8b6c8a5c8191f01e0cb1
bchanx <bchanx@gmail.com>
2015-10-12 04:04:19 -0700

    Add color selection

--- web/static/uncompiled/js/demo/gameboy-css.js
+++ web/static/uncompiled/js/demo/gameboy-css.js
@@ -4,13 +4,19 @@
  */
 
 $(function() {
-  console.log('hi');
 
-  $('#button').bind('click', function() {
-    $('#gameboy').hide(50, function() {
-      setTimeout(function() {
-        $('#gameboy').show();
-      }, 100);
-    });
+  var colors = ['red', 'purple', 'green', 'yellow', 'teal', 'transparent'];
+
+  $('.color-selection').bind('click', function(element) {
+    $('.color-selection').removeClass('active');
+    var color = $(element.target).addClass('active').data('color');
+    $('#gameboy').css('opacity', 0).removeClass(colors.join(' ')).addClass(color);
+    var clone = $('#gameboy').clone(true);
+    clone.css('opacity', '');
+    $('#gameboy').remove();
+    $('#colors').before(clone);
+    $('#gameboy').fadeIn(100);
   });
+
+  $('.color-selection[data-color="green"]').click();
 });
