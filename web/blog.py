9837426af37293ee9c568dea7d0f542329f33384
bchanx <bchanx@gmail.com>
2015-10-18 23:02:32 -0700

    Add demo

--- web/blog.py
+++ web/blog.py
@@ -16,6 +16,30 @@ def logosInPureCSS():
 def logosInPureCSS2():
   return redirect(url_for('logosInPureCSS'))
 
+def gameboyCode():
+  code = {}
+  code['processor'] = {
+    'html': '<div id="processor"></div>',
+    'css': """#processor {
+  position: absolute;
+  width: 56px;
+  height: 56px;
+  top: 285px;
+  left: 142px;
+  z-index: 4;
+  background-color: #222222;
+  color: #ffffff;
+  font-size: 15px;
+  letter-spacing: 3px;
+  font-weight: 700;
+  font-family: 'Oxygen', Helvetica, arial, sans-serif;
+  -webkit-animation: processor 8s infinite linear;
+  animation: processor 8s infinite linear;
+}"""
+  }
+  return code
+
+
 @app.route('/animated-gameboy-in-css-blog')
 def gameboyCSSBlog():
-  return render('blog/animated-gameboy-in-css-blog.html', css='gameboy-blog.less')
+  return render('blog/animated-gameboy-in-css-blog.html', css='gameboy-blog.less', js='blog/gameboy-blog.js', code=gameboyCode())
---
+++ web/static/uncompiled/js/blog/gameboy-blog.js
@@ -0,0 +1,5 @@
+console.log("YO...");
+
+$(function() {
+  hljs.initHighlightingOnLoad();
+});
