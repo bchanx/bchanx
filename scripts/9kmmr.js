5ab3b7f41e1cd261818e350ec1f2f4e4b94d3597
bchanx <bchanx@gmail.com>
2017-11-23 02:21:35 -0800

    In progress commit

--- scripts/9kmmr.js
+++ scripts/9kmmr.js
@@ -1,17 +1,40 @@
 const DATE_RE = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}/;
 
+const SOCIAL_TYPES = {
+  liquipedia: 1,
+  dotabuff: 2,
+  twitter: 4,
+  facebook: 5,
+  instagram: 6,
+  twitch: 3,
+};
+
+const SORT_TYPES = {
+  DATE_ACHIEVED: 'dateAchieved',
+  MOST_RECENT: 'mostRecent',
+  ALPHABETICALLY: 'alphabetically'
+};
+
 Vue.component('player-tile', {
   props: ['player', 'index'],
   template: `
-    <div class="player-tile">
-      <div>
-        <span class="index">{{index}}. </span>
-        <span class="name">{{player.name}}</span>
+    <div class="player-tile" :class="{ 'not-verified' : !player.isVerified }" @click="hasSource ? showSource() : null">
+      <div class="player-index">{{index}}.</div>
+      <div class="player-image" v-if="player.image">
+        <img :src="player.image"/>
       </div>
-      <div>
-        <span class="date">{{dateAchieved}}</span>
-        <span class="region">{{player.region}}</span>
-        <span class="role">{{player.role}}</span>
+      <div class="player-details">
+        <div class="player-profile">
+          <span class="name">{{player.name}}</span>
+          <span class="date">{{dateAchieved}}</span>
+        </div>
+        <div class="player-metadata">
+          <span class="region">{{player.region}}</span>
+          <span class="role">{{player.role}}</span>
+          <span class="social" v-if="socialTypes.length">
+            <a :class="socialType" :href="player.social[socialType]" @click.stop v-for="socialType in socialTypes" target="_blank"></a>
+          </span>
+        </div>
       </div>
     </div>
   `,
@@ -25,6 +48,21 @@ Vue.component('player-tile', {
         }
       }
       return date;
+    },
+    socialTypes: function() {
+      return Object.keys(this.player.social || {}).filter(x => !!SOCIAL_TYPES[x]).sort((a, b) => {
+        a = SOCIAL_TYPES[a];
+        b = SOCIAL_TYPES[b];
+        return a > b ? 1 : a < b ? -1 : 0;
+      });
+    },
+    hasSource: function() {
+      return !!(this.player.source || []).length;
+    }
+  },
+  methods: {
+    showSource: function() {
+      console.log("-->> show source!!");
     }
   }
 });
@@ -37,18 +75,74 @@ const APP = new Vue({
         Navigation
       </div>
       <div class="filters">
-        Filters
-        <input v-model.trim="filterInput"/>
+        <div class="filter-header">
+          <div class="filter-toggle" @click="toggleShowFilters">
+            Filters
+            <span :class="[showFilters ? 'ion-ios-arrow-up' : 'ion-ios-arrow-down']"></span>
+          </div>
+          <div class="filter-reset">
+            <span class="action" v-if="hasSetFilters" @click="resetFilters">reset</span>
+          </div>
+        </div>
+        <div class="filter-types" v-if="showFilters">
+          <div class="input-filters" v-if="showFilters">
+            <div class="filter-option">
+              <div class="filter-label">username</div>
+              <div class="filter-input">
+                <input v-model.trim="filters.input"/>
+                <span class="filter-input-clear action ion-close" v-if="filters.input.length" @click="clearInput"></span>
+              </div>
+            </div>
+            <div class="filter-option">
+              <div class="filter-label">region</div>
+              <div class="filter-input">
+                <div class="region"
+                  :class="{ active: filters.region === region }"
+                  v-for="region in regionTypes"
+                  :key="region"
+                  @click="toggleFilter('region', region)">
+                  {{region}}
+                </div>
+              </div>
+            </div>
+            <div class="filter-option">
+              <div class="filter-label">role</div>
+              <div class="filter-input">
+                <div class="role"
+                  :class="{ active: filters.role === role }"
+                  v-for="role in roleTypes"
+                  :key="role"
+                  @click="toggleFilter('role', role)">
+                  {{role}}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="sort-filters">
+            <div class="filter-option">
+              <input type="radio" :id="constants.SORT_TYPES.DATE_ACHIEVED" :value="constants.SORT_TYPES.DATE_ACHIEVED" v-model="filters.sort"/>
+              <label :for="constants.SORT_TYPES.DATE_ACHIEVED">Date Achieved</label>
+            </div>
+            <div class="filter-option">
+              <input type="radio" :id="constants.SORT_TYPES.MOST_RECENT" :value="constants.SORT_TYPES.MOST_RECENT" v-model="filters.sort"/>
+              <label :for="constants.SORT_TYPES.MOST_RECENT">Most Recent</label>
+            </div>
+            <div class="filter-option">
+              <input type="radio" :id="constants.SORT_TYPES.ALPHABETICALLY" :value="constants.SORT_TYPES.ALPHABETICALLY" v-model="filters.sort"/>
+              <label :for="constants.SORT_TYPES.ALPHABETICALLY">Alphabetically</label>
+            </div>
+          </div>
+        </div>
       </div>
       <div class="results">
-        <div v-if="isLoading">
+        <div v-if="isLoading" class="placeholder">
           Loading...
         </div>
-        <div v-else-if="!players.length">
+        <div v-else-if="!players.length" class="placeholder">
           No player info available.
         </div>
-        <div v-else-if="!filteredPlayers.length">
-          No players found.
+        <div v-else-if="!filteredPlayers.length" class="placeholder">
+          No player matches found.
         </div>
         <div v-else class="player-list">
           <template v-for="(player, index) in filteredPlayers">
@@ -62,9 +156,19 @@ const APP = new Vue({
     </div>
   `,
   data: {
+    constants: {
+      SORT_TYPES: SORT_TYPES,
+      SOCIAL_TYPES: SOCIAL_TYPES
+    },
     isLoading: false,
     players: [],
-    filterInput: ''
+    showFilters: true,
+    filters: {
+      input: '',
+      region: '',
+      role: '',
+      sort: SORT_TYPES.DATE_ACHIEVED
+    }
   },
   mounted: function() {
     this.isLoading = true;
@@ -74,17 +178,57 @@ const APP = new Vue({
         this.isLoading = false;
         if (!err && res.ok) {
           // TODO: sort by date and add a global rank
-          this.players = res.body;
+          let players = res.body;
+          // Sanitize
+          players.forEach(p => {
+            p.region = (p.region || '').toLowerCase();
+            p.role = (p.role || '').toLowerCase();
+          });
+          this.players = players;
         }
       });
+  },
+  methods: {
+    toggleShowFilters: function() {
+      this.showFilters = !this.showFilters;
+    },
+    toggleFilter: function(type, name) {
+      this.$set(this.filters, type, this.filters[type] === name ? '' : name);
+    },
+    resetFilters: function() {
+      Object.keys(this.filters).forEach(filterType => {
+        this.$set(this.filters, filterType, filterType === 'sort' ? SORT_TYPES.DATE_ACHIEVED : '');
+      });
+    },
+    clearInput: function() {
+      this.$set(this.filters, 'input', '');
+    }
   },
   computed: {
     filteredPlayers: function() {
       let players = this.players;
-      let filterInput = this.filterInput.toLowerCase();
+      let input = this.filters.input.toLowerCase();
+      let region = this.filters.region;
+      let role = this.filters.role;
+
       return this.players.filter(p => {
-        return (p.name || '').toLowerCase().indexOf(filterInput) >= 0;
+        return (!region || p.region === region) &&
+          (!role || p.role === role) &&
+          (p.name || '').toLowerCase().indexOf(input) >= 0;
       });
+    },
+    hasSetFilters: function() {
+      return Object.keys(this.filters).filter(x => x === 'sort' ? this.filters[x] !== SORT_TYPES.DATE_ACHIEVED : !!this.filters[x]).length;
+    },
+    regionTypes: function() {
+      let players = this.players;
+      let regions = [... new Set(this.players.map(p => p.region.toLowerCase()))].sort();
+      return regions;
+    },
+    roleTypes: function() {
+      let players = this.players;
+      let roles = [... new Set(this.players.map(p => p.role.toLowerCase()))].sort();
+      return roles;
     }
   }
 });
